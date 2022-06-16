e5fb24334ce716baa2e377959de95c1d1f8492b2
Dmitrii Sobin <neo7976@yandex.ru>
2022-06-16 12:21:27 +0300

    Update

--- src/NoGit.java
+++ src/NoGit.java
@@ -1,6 +1,22 @@
 public class NoGit {
 
+    private String gitName;
+
+    public NoGit(String gitName) {
+        this.gitName = gitName;
+    }
+
+    public String getGitName() {
+        return gitName;
+    }
+
+    public void setGitName(String gitName) {
+        this.gitName = gitName;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Тестируем новый класс");
+        System.out.println("Тестируем новый класс и не только");
+
+
     }
 }
